b04d38cfbc9030f6cec46cbef4a90c17a32cd29a
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2020-11-03 14:34:16 +0100

    Folder restructure, add comments for what to do in normalization

--- feature_data_normalize.py
+++ feature_data_normalize.py
@@ -1,1 +1,13 @@
 # resize image to some size, then resize bboxes with same ratio
+
+# width = xxx
+# height = xxx
+
+# for each image
+#   if annotation exists
+#       get width, height ratio for resize
+#       resize images
+#       round(num, decimals) annotations by ratio
+#
+#       save annotations to annotations/normalized_transformed
+#       save images to images/normalized
