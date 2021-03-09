6b209b6f8a7e6c86f9a904a0210e740d505bd070
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2021-03-09 20:30:53 +0000

    Fix bug

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 
 app = Flask(__name__)
 
-PORT = os.getenv("PORT", "8080")
-MODEL_PATH = os.getenv("MODEL_PATH", "/models/model.h5")
+PORT = os.getenv("PORT", "8090")
+MODEL_PATH = os.getenv("MODEL_PATH", "/home/red/code/face_detection/models/base_model_v6.h5")
 print ('Will listen on port:', PORT, '\nAnd load model at path:', MODEL_PATH)
 
 global model
@@ -17,11 +17,12 @@ model = keras.models.load_model(MODEL_PATH)
 @app.route("/predict", methods=["GET"])
 def process_image():
     file = request.files['image']
+    
     image = np.array(img_to_mat(file.stream), ndmin=4)
 
     pred = model.predict(image)
     
-    return 'Confidence that image matches pattern model was trained for:\n', pred[0]
+    return ('Confidence that model has detected object it was trained for:\n' + str(pred[0]))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=PORT)
